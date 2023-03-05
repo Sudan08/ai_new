642ff59826ada4bd2b6c708f38db8381658540f5
sNishant011 <shresthanishant011@gmail.com>
2023-03-05 21:04:00 +0545

    fix: navbar

---
+++ tailwind.config.cjs
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["InterVariable", "Inter"],
+      },
+      colors: {
+        brand: {
+          100: "#f8fff0",
+          200: "#ebf2e4",
+          300: "#d2e6be",
+          400: "#b3d991",
+          500: "#93cc68",
+          600: "#74c043",
+          700: "#55992e",
+          800: "#39731d",
+          900: "#224d0f"
+        }
+      }
+    },
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
