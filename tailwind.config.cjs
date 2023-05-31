b76111bb481ccd164bc0454a611c731fdff69ace
Sudan Shakya <sudanshakya08@gmail.com>
2023-05-31 16:18:13 +0545

    feat:added insta component and fixed footer background

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,9 +3,6 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
-      fontFamily: {
-        sans: ['Helvetica', 'Arial', 'sans-serif'],
-      },
       colors: {
         brand: {
           100: "#f8fff0",
